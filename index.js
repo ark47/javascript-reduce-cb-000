0f4e8159be5c7b707e85d8c496ff6ceecac7c204
Chris Nunez <cenunez47@gmail.com>
2018-04-09 01:45:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const app = "I don't do much."
 
+function reduce(array, callback, initialValue) {
+	
+	let result = initialValue;
+	
+	array.forEach((item, index) => {
+		result = callback(result, item, index, array);
+	});
+
+	return result;
+}
